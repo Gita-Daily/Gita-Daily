e4f8eaea1c504e037570af446bf8e9dd279a43fa
Manas <manaslegodesigns@gmail.com>
2022-04-26 23:05:24 +0530

    adds return text message function

--- bg_bot.py
+++ bg_bot.py
@@ -4,6 +4,9 @@ import time
 import pause
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
+import requests
+import json
+import urllib.request
 
 app = Flask(__name__)
 
@@ -33,21 +36,50 @@ def runserver():
         phone_no = msg['key']['remoteJid'][:12]
 
         if phone_no not in users.keys() and ( msg_text.lower().strip() == 'hare krishna' or msg_text.lower().strip() == 'hare krisna' or msg_text.lower().strip() == 'hare krsna'):
-            users[phone_no] = [0, True, name]
+            users[phone_no] = [1, True, name]
 
 
 
 
     print(users)
-    return "Hello, World!"
+    return 'Hello, World!'
+
+chapter_shlokas = [47, 72, 43, 42, 29, 47, 30, 28, 34, 42, 55, 20, 34, 27, 20, 24, 28, 78]
 
 def send_message():
-    print('test')
+    for phone_no in users.keys():
+        user_data = users[phone_no];
+        if(user_data[1]):
+            ch, sh = getChSh(user_data[0]);
+            URL = 'https://bhagavadgitaapi.in/slok/{}/{}'.format(ch, sh)
+            page = requests.get(URL)
+
+            result = json.loads(page.text)
+
+            message_text = result['slok'] + '\n\n' + result['transliteration'] + '\n\nCommentary by ' + result['sankar']['author'] + '\n\n' + result['sankar']['et'] + '\n\n' + result['sankar']['ht']
+
+            return_webhook_url = 'https://betablaster.in/api/send.php?number=919606807941&type=text&message={}&instance_id=62680FA67B740&access_token=f308a4f4a94bfd9ba008a559c5019d41'.format(message_text)
+            urllib.request.urlopen(return_webhook_url)
+
+
+
+def getChSh(n):
+    ch = 1
+    for n_schlokas in chapter_shlokas:
+        if(n-n_schlokas > 0):
+            n = n - n_schlokas
+            ch = ch + 1
+    
+    return (ch, n);
+
+
+        
+
 
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func='send_message', trigger="interval", seconds=10)
+scheduler.add_job(func='send_message', trigger="interval", seconds=25)
 
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
