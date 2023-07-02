487991ff3e5c1f12bf5da2d1c961fb97c7365432
Ubuntu <ubuntu@ip-172-31-28-73.ap-southeast-1.compute.internal>
2023-07-02 18:38:15 +0000

    prepare for hindi v1

--- app.js
+++ app.js
@@ -47,15 +47,15 @@ async function sendAudioMessage(uniqueID, filePath) {
 
 function removeUnnecessarySpaces(str) {
   // Split the string by '\r\n' to get an array of strings
-  let arr = str.split('\r\n');
+  let arr = str.split("\r\n");
 
   // Iterate over the array and trim each string
   for (let i = 0; i < arr.length; i++) {
-      arr[i] = arr[i].trim();
+    arr[i] = arr[i].trim();
   }
 
   // Join the array back into a single string with '\r\n' as the separator
-  return arr.join('\r\n');
+  return arr.join("\r\n");
 }
 
 function checkPhoneNoExists(phoneNo, data) {
@@ -67,12 +67,12 @@ function checkPhoneNoExists(phoneNo, data) {
   }
 }
 
-function addUserToData(data, userID, name, number, booleanValue) {
+function addUserToData(data, userID, name, number, booleanValue, language) {
   try {
     if (!data) {
       data = {};
     }
-    data[userID] = [name, number, booleanValue];
+    data[userID] = [name, number, booleanValue, language];
     fs.writeFile("data.json", JSON.stringify(data), (err) => {
       if (err) throw err;
     });
@@ -99,7 +99,6 @@ function getChSh(n) {
   return [ch, n];
 }
 
-
 function sendShlok() {
   try {
     let count = 0;
@@ -110,41 +109,76 @@ function sendShlok() {
       for (let uniqueID of keys) {
         let user_data = data[uniqueID];
         if (user_data[2]) {
-          let [ch, sh] = getChSh(user_data[1]);
-          let jsonData = JSON.parse(
-            fs.readFileSync(`${ch}/${sh}.json`, "utf8")
-          );
-          let message_text = "";
-          if (jsonData["commentary"] === "NONE") {
-            message_text = "*Chapter " + ch + " Verse " + sh + "*\n\n" +
-              jsonData["verse"].slice(0, -1) +
-              "\n\n*Transliteration*\n" +
-              jsonData["transliteration"] +
-              "\n\n*Word Meanings*" +
-              jsonData["word meanings"] +
-              "\n*Translation*\n" +
-              removeUnnecessarySpaces(jsonData["translation"])
+          if (user_data[3] == "english") {
+            let [ch, sh] = getChSh(user_data[1]);
+            let jsonData = JSON.parse(
+              fs.readFileSync(`${ch}/${sh}.json`, "utf8")
+            );
+            let message_text = "";
+            if (jsonData["commentary"] === "NONE") {
+              message_text =
+                "*Chapter " +
+                ch +
+                " Verse " +
+                sh +
+                "*\n\n" +
+                jsonData["verse"].slice(0, -1) +
+                "\n\n*Transliteration*\n" +
+                jsonData["transliteration"] +
+                "\n\n*Word Meanings*" +
+                jsonData["word meanings"] +
+                "\n*Translation*\n" +
+                removeUnnecessarySpaces(jsonData["translation"]);
+            } else {
+              message_text =
+                "*Chapter " +
+                ch +
+                " Verse " +
+                sh +
+                "*\n\n" +
+                jsonData["verse"].slice(0, -1) +
+                "\n\n*Transliteration*\n" +
+                jsonData["transliteration"] +
+                "\n\n*Word Meanings*" +
+                jsonData["word meanings"] +
+                "\n*Translation*\n" +
+                removeUnnecessarySpaces(jsonData["translation"]) +
+                "\n*Commentary*" +
+                jsonData["commentary"];
+            }
+            message_text +=
+              '\nThank you for reading today\'s shlok. You can encourage your friends and family to also start reading the Gita by sharing this message:\n\n🦚🦚 To receive daily Bhagavad Gita shlokas, click this link: https://api.whatsapp.com/send/?phone=919036504927&text=Hare%20Krishna or WhatsApp "Hare Krishna" to +919036504927 🦚🦚\n\nDeveloped by the creative minds at Gita Daily, and brought to you in partnership with Do Yoga. Visit us at https://www.gitadaily.in and https://do.yoga/ to learn more.';
+            await sendMessage(uniqueID, message_text);
+            let next_shlok = jsonData["next shlok"];
+            for (let i = sh; i < next_shlok; i++) {
+              let audio_file = `Audio/${ch}_${i}.mp3`;
+              await sendAudioMessage(uniqueID, audio_file);
+            }
+            data[uniqueID][1] = data[uniqueID][1] + next_shlok - sh;
           } else {
-            message_text = "*Chapter " + ch + " Verse " + sh + "*\n\n" +
-              jsonData["verse"].slice(0, -1) +
-              "\n\n*Transliteration*\n" +
-              jsonData["transliteration"] +
-              "\n\n*Word Meanings*" +
-              jsonData["word meanings"] +
+            let [ch, sh] = getChSh(user_data[1]);
+            let jsonData = JSON.parse(
+              fs.readFileSync(`hindi_${ch}/${sh}.json`, "utf8")
+            );
+            let message_text = "";
+            message_text =
+              "*Chapter " +
+              ch +
+              " Verse " +
+              sh +
+              "*\n\n" +
+              jsonData["shlok"] +
               "\n*Translation*\n" +
-              removeUnnecessarySpaces(jsonData["translation"]) +
+              jsonData["translation"] +
               "\n*Commentary*" +
               jsonData["commentary"];
-          }
-          message_text +=
-            '\nThank you for reading today\'s shlok. You can encourage your friends and family to also start reading the Gita by sharing this message:\n\n🦚🦚 To receive daily Bhagavad Gita shlokas, click this link: https://api.whatsapp.com/send/?phone=919036504927&text=Hare%20Krishna or WhatsApp "Hare Krishna" to +919036504927 🦚🦚\n\nDeveloped by the creative minds at Gita Daily, and brought to you in partnership with Do Yoga. Visit us at https://www.gitadaily.in and https://do.yoga/ to learn more.';
-          await sendMessage(uniqueID, message_text);
-          let next_shlok = jsonData["next shlok"];
-          for (let i = sh; i < next_shlok; i++) {
+            message_text +=
+              '\nआज के श्लोक को पढ़ने के लिए धन्यवाद। आप इस संदेश को साझा करके अपने दोस्तों और परिवार को भी गीता पढ़ने के लिए प्रेरित कर सकते हैं:\n\n🦚🦚 रोजाना भगवद गीता के श्लोक प्राप्त करने के लिए, इस लिंक पर क्लिक करें: https://api.whatsapp.com/send/?phone=919036504927&text=Hare%20Krishna या WhatsApp पर "Hare Krishna" लिखकर +919036504927 पर भेजें 🦚🦚\n\nयह सेवा Gita Daily के रचनात्मक दिमागों द्वारा विकसित की गई है, और यह आपके लिए Do Yoga के साथ साझेदारी में प्रस्तुत की जा रही है। हमसे अधिक जानने के लिए https://www.gitadaily.in और https://do.yoga/ पर जाएं।';
+            await sendMessage(uniqueID, message_text);
             let audio_file = `Audio/${ch}_${i}.mp3`;
             await sendAudioMessage(uniqueID, audio_file);
+            data[uniqueID][1] = data[uniqueID][1] + 1;
           }
-          data[uniqueID][1] = data[uniqueID][1] + next_shlok - sh;
         }
         count += 1;
       }
@@ -161,68 +195,103 @@ function sendShlok() {
         "No of Shloks sent today: " + count.toString()
       );
       sendMessage(
-	"918303566096@c.us",
-	"No of Shloks sent today: " + count.toString()
+        "918303566096@c.us",
+        "No of Shloks sent today: " + count.toString()
       );
       fs.writeFileSync("data.json", JSON.stringify(data, null, 2), "utf8");
-
     });
-        // setTimeout(sendShlok, 24 * 60 * 60 * 1000);
+    // setTimeout(sendShlok, 24 * 60 * 60 * 1000);
   } catch (e) {
     console.error(e);
   }
 }
 
-function nextShlok(uniqueID) {
+async function nextShlok(uniqueID) {
   try {
     let data = JSON.parse(fs.readFileSync("data.json", "utf8"));
     let user_data = data[uniqueID];
-    let [ch, sh] = getChSh(user_data[1]);
-    let jsonData = JSON.parse(fs.readFileSync(`${ch}/${sh}.json`, "utf8"));
-    let message_text = "";
-    if (jsonData["commentary"] === "NONE") {
-      message_text = "*Chapter " + ch + " Verse " + sh + "*\n\n" +
-        jsonData["verse"].slice(0, -1) +
-        "\n\n*Transliteration*\n" +
-        jsonData["transliteration"] +
-        "\n\n*Word Meanings*" +
-        jsonData["word meanings"] +
-        "\n*Translation*\n" +
-        removeUnnecessarySpaces(jsonData["translation"])
-    } else {
-      message_text = "*Chapter " + ch + " Verse " + sh + "*\n\n" +
-        jsonData["verse"].slice(0, -1) +
-        "\n\n*Transliteration*\n" +
-        jsonData["transliteration"] +
-        "\n\n*Word Meanings*" +
-        jsonData["word meanings"] +
-        "\n*Translation*\n" +
-        removeUnnecessarySpaces(jsonData["translation"]) +
-        "\n*Commentary*" +
-        jsonData["commentary"];
+    if (user_data[2]) {
+      if (user_data[3] == "english") {
+        let [ch, sh] = getChSh(user_data[1]);
+        let jsonData = JSON.parse(fs.readFileSync(`${ch}/${sh}.json`, "utf8"));
+        let message_text = "";
+        if (jsonData["commentary"] === "NONE") {
+          message_text =
+            "*Chapter " +
+            ch +
+            " Verse " +
+            sh +
+            "*\n\n" +
+            jsonData["verse"].slice(0, -1) +
+            "\n\n*Transliteration*\n" +
+            jsonData["transliteration"] +
+            "\n\n*Word Meanings*" +
+            jsonData["word meanings"] +
+            "\n*Translation*\n" +
+            removeUnnecessarySpaces(jsonData["translation"]);
+        } else {
+          message_text =
+            "*Chapter " +
+            ch +
+            " Verse " +
+            sh +
+            "*\n\n" +
+            jsonData["verse"].slice(0, -1) +
+            "\n\n*Transliteration*\n" +
+            jsonData["transliteration"] +
+            "\n\n*Word Meanings*" +
+            jsonData["word meanings"] +
+            "\n*Translation*\n" +
+            removeUnnecessarySpaces(jsonData["translation"]) +
+            "\n*Commentary*" +
+            jsonData["commentary"];
+        }
+        message_text +=
+          '\nThank you for reading today\'s shlok. You can encourage your friends and family to also start reading the Gita by sharing this message:\n\n🦚🦚 To receive daily Bhagavad Gita shlokas, click this link: https://api.whatsapp.com/send/?phone=919036504927&text=Hare%20Krishna or WhatsApp "Hare Krishna" to +919036504927 🦚🦚\n\nDeveloped by the creative minds at Gita Daily, and brought to you in partnership with Do Yoga. Visit us at https://www.gitadaily.in and https://do.yoga/ to learn more.';
+        await sendMessage(uniqueID, message_text);
+        let next_shlok = jsonData["next shlok"];
+        for (let i = sh; i < next_shlok; i++) {
+          let audio_file = `Audio/${ch}_${i}.mp3`;
+          await sendAudioMessage(uniqueID, audio_file);
+        }
+        data[uniqueID][1] = data[uniqueID][1] + next_shlok - sh;
+      } else {
+        let [ch, sh] = getChSh(user_data[1]);
+        let jsonData = JSON.parse(
+          fs.readFileSync(`hindi_${ch}/${sh}.json`, "utf8")
+        );
+        let message_text = "";
+        message_text =
+          "*Chapter " +
+          ch +
+          " Verse " +
+          sh +
+          "*\n\n" +
+          jsonData["shlok"] +
+          "\n*Translation*\n" +
+          jsonData["translation"] +
+          "\n*Commentary*" +
+          jsonData["commentary"];
+        message_text +=
+          '\nआज के श्लोक को पढ़ने के लिए धन्यवाद। आप इस संदेश को साझा करके अपने दोस्तों और परिवार को भी गीता पढ़ने के लिए प्रेरित कर सकते हैं:\n\n🦚🦚 रोजाना भगवद गीता के श्लोक प्राप्त करने के लिए, इस लिंक पर क्लिक करें: https://api.whatsapp.com/send/?phone=919036504927&text=Hare%20Krishna या WhatsApp पर "Hare Krishna" लिखकर +919036504927 पर भेजें 🦚🦚\n\nयह सेवा Gita Daily के रचनात्मक दिमागों द्वारा विकसित की गई है, और यह आपके लिए Do Yoga के साथ साझेदारी में प्रस्तुत की जा रही है। हमसे अधिक जानने के लिए https://www.gitadaily.in और https://do.yoga/ पर जाएं।';
+        await sendMessage(uniqueID, message_text);
+        let audio_file = `Audio/${ch}_${i}.mp3`;
+        await sendAudioMessage(uniqueID, audio_file);
+        data[uniqueID][1] = data[uniqueID][1] + 1;
+      }
     }
-    message_text +=
-    '\nThank you for reading today\'s shlok. You can encourage your friends and family to also start reading the Gita by sharing this message:\n\n🦚🦚 To receive daily Bhagavad Gita shlokas, click this link: https://api.whatsapp.com/send/?phone=919036504927&text=Hare%20Krishna or WhatsApp "Hare Krishna" to +919036504927 🦚🦚\n\nDeveloped by the creative minds at Gita Daily, and brought to you in partnership with Do Yoga. Visit us at https://www.gitadaily.in and https://do.yoga/ to learn more.';
-    sendMessage(uniqueID, message_text);
-    let next_shlok = jsonData["next shlok"];
-
-    for (let i = sh; i < next_shlok; i++) {
-      let audio_file = `Audio/${ch}_${i}.mp3`;
-      sendAudioMessage(uniqueID, audio_file);
-    }
-    data[uniqueID][1] = data[uniqueID][1] + next_shlok - sh;
     fs.writeFileSync("data.json", JSON.stringify(data, null, 2), "utf8");
   } catch (e) {
     console.error(e);
   }
 }
 
-function sendGeneralMessage(msg) {
+async function sendGeneralMessage(msg) {
   try {
     let data = JSON.parse(fs.readFileSync("data.json", "utf8"));
     let keys = Object.keys(data);
     for (let uniqueID of keys) {
-      sendMessage(uniqueID, msg);
+      await sendMessage(uniqueID, msg);
     }
   } catch (e) {
     console.error(e);
@@ -252,7 +321,6 @@ setTimeout(() => {
   setInterval(sendShlok, 24 * 60 * 60 * 1000);
 }, timeUntil5am);
 
-
 client.on("message", (message) => {
   try {
     fs.readFile("data.json", "utf8", async (err, content) => {
@@ -270,12 +338,26 @@ client.on("message", (message) => {
       ];
       const nxtShlok = ["next shlok", "next verse", "next slok", "next shloka"];
 
+      if(messageBody && messageBody.toLowerCase().startsWith("hindi")) {
+        if (!checkPhoneNoExists(userID, data)) {
+          addUserToData(data, userID, name, 1, true, "hindi");
+        } else {
+          addUserToData(data, userID, name, data[userID][1], true, "hindi");
+        }        
+        client.sendMessage(
+          message.from,
+          "*🦚Hare Krishna " +
+            name +
+            '!🦚* \n\nYou have successfully changed your language to Hindi.'
+        );            
+      }
       if (messageBody && messageBody.toLowerCase().startsWith("tardis")) {
-       sendShlok();
+        sendShlok();
       }
       if (messageBody && messageBody.toLowerCase().startsWith(secretString)) {
         sendGeneralMessage(messageBody.replace(secretString, ""));
-      } else if ( messageBody && 
+      } else if (
+        messageBody &&
         feedbackStrings.some((spelling) =>
           messageBody.toLowerCase().includes(spelling)
         )
@@ -296,13 +378,15 @@ client.on("message", (message) => {
           userID,
           'Thank you for your valuable feedback! Your thoughts and opinions help us improve and better serve our community. As the Bhagavad Gita states, "Karmanye vadhikaraste ma phaleshu kadachana" (2.47), which means "In actions, do as you will, but do not be attached to their results." Your feedback is a reminder for us to stay humble and continue our journey towards growth and improvement. Thank you again!'
         );
-      } else if ( messageBody &&
+      } else if (
+        messageBody &&
         nxtShlok.some((spelling) =>
           messageBody.toLowerCase().includes(spelling)
         )
       ) {
         nextShlok(userID);
-      } else if ( messageBody &&
+      } else if (
+        messageBody &&
         spellings.some((spelling) =>
           messageBody.toLowerCase().includes(spelling)
         )
@@ -318,13 +402,19 @@ client.on("message", (message) => {
             name +
             '!🦚* \n\nYou are now subscribed to receive daily *Bhagvad Gita* shlokas ✅ \n\nYou will receive a message everyday at *7:00 AM* ⏰ \n\nIf you wish to start from the very beginning, simply message us with "shloka 1". To request the next shloka at any time, send us "next shloka".\n\nWe welcome and value your feedback. If you have any suggestions or comments, please message us "feedback: followed by your thoughts".\n\nShould you ever wish to unsubscribe, you can do so at any time by sending "unsubscribe" to this number.\n\nYour journey of self realisation starts now 🙏. Let\'s delve deeper into the wisdom of the Bhagavad Gita together.\n\nThis service was developed by the creative minds at Gita Daily and is brought to you in partnership with Do Yoga. To learn more about us, visit https://www.gitadaily.in and https://do.yoga/'
         );
-      } else if (messageBody && messageBody.toLowerCase().includes("unsubscribe")) {
+      } else if (
+        messageBody &&
+        messageBody.toLowerCase().includes("unsubscribe")
+      ) {
         addUserToData(data, userID, name, data[userID][1], false);
         client.sendMessage(
           message.from,
           'You have been unsubscribed from Bhagavad Gita notifications.\n\nYou can resubscribe anytime by sending "hare krishna" to this number.\n\nWe thank you for taking the time in starting your journey of self realisation and we hope you will come back soon 🙏 \n\nPlease help us by sharing your feedback here 👇\nhttps://forms.gle/pLm2fczXNfKXk8dn7'
         );
-      } else if (messageBody && messageBody.toLowerCase().includes("shloka 1")) {
+      } else if (
+        messageBody &&
+        messageBody.toLowerCase().includes("shloka 1")
+      ) {
         addUserToData(data, userID, name, 1, true);
         message_text =
           "Great choice! You have decided to start fresh from the beginning of the Bhagavad Gita. Your profile has been updated and you will now receive daily messages starting from Shloka 1. We hope that this journey through the Bhagavad Gita will bring you wisdom, inspiration, and guidance in your life. If you have any questions or concerns, please do not hesitate to reach out to us at +917337610771 or hi@do.yoga \n\nThank you for choosing to embark on this journey with us. Hare Krishna!";
--- restart_pm2.sh
+++ restart_pm2.sh
@@ -2,5 +2,5 @@
 echo "Stopping PM2"
 pm2 stop 0
 echo "Starting PM2"
-pm2 start 0
+pm2 start app.js
 echo "PM2 restart complete at $(date)"
